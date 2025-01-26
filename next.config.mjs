1a9fe7afe265ab36968086a7294b5f634da88243
MahnoorJavaidAkhtar <mahnoortaha59@gmail.com>
2025-01-26 14:56:50 +0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {images: {
+    domains: ["cdn.sanity.io"], // Sanity's CDN domain
+  },};
+  
 export default nextConfig;
+  
